6b481781a62b89380dbec79cb468c2e44ec3bd8d
Kajetan Jóźwiak <unvios22@gmail.com>
2020-07-20 01:10:42 +0200

    WIP: Add particle systems

---
+++ Assets/Scripts/PlayerController/FPCameraController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+namespace Controllers {
+	public class FPCameraController : MonoBehaviour {
+   
+		[SerializeField] private float mouseSensitivity;
+	
+		[SerializeField] private float maxXTilt = 80f;
+		[SerializeField] private float minXTilt = -80f;
+		[SerializeField] private bool invertMouseYRot;
+	
+		private float _totalXRotCache;
+		private Vector2 _inputCameraRotation;
+
+		private void LateUpdate() {
+			var mouseInput = ReadMouseInput();
+			_inputCameraRotation = ConvertMouseInputToCameraAxisRotations(mouseInput);
+			AccountForMouseYInversion();
+			CacheTotalXRotation();
+			ClampCameraXRotToTiltConstraints();
+			ApplyRotation();
+		}
+
+		private Vector2 ReadMouseInput() {
+			var xMouseInputRot = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
+			var yMouseInputRot = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;
+			return new Vector2(xMouseInputRot, yMouseInputRot);
+		}
+
+		private Vector2 ConvertMouseInputToCameraAxisRotations(Vector2 mouseInput) {
+			return new Vector2(mouseInput.y, mouseInput.x);
+		}
+
+		private void AccountForMouseYInversion() {
+			_inputCameraRotation.x = invertMouseYRot ? _inputCameraRotation.x : -_inputCameraRotation.x;
+		}
+
+		private void CacheTotalXRotation() {
+			_totalXRotCache += _inputCameraRotation.x;
+		}
+	
+		private void ApplyRotation() {
+			var localCameraRotation = transform.localEulerAngles;
+			var totalXRot = localCameraRotation.x + _inputCameraRotation.x;
+			var totalYRot = localCameraRotation.y + _inputCameraRotation.y;
+			transform.localRotation = Quaternion.Euler(totalXRot,totalYRot,localCameraRotation.z);
+		}
+
+		private void ClampCameraXRotToTiltConstraints() {
+			//camera wants to look too low
+			if (_totalXRotCache > maxXTilt) {
+				_inputCameraRotation.x -= _totalXRotCache - maxXTilt;
+				_totalXRotCache = maxXTilt;
+			}
+			//camera wants to look too high
+			if (_totalXRotCache < minXTilt) {
+				_inputCameraRotation.x += minXTilt - _totalXRotCache;
+				_totalXRotCache = minXTilt;
+			}
+		}
+
+		private void OnValidate() {
+			maxXTilt = Mathf.Clamp(maxXTilt, 0f, 90f);
+			minXTilt = Mathf.Clamp(minXTilt, -90f, 0);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+
+namespace Controllers {
+	public class PlayerController : MonoBehaviour {
+		[SerializeField] private float moveSpeed = 10;
+		[SerializeField] private float jumpForce = 5;
+		[SerializeField] private float groundCheckDistance;
+		[SerializeField] private FPCameraController playerCamera;
+		[SerializeField] private Transform cameraAttachPoint;
+		[SerializeField] private Transform groundChecker;
+	
+		[SerializeField] private bool drawGroundCheckSphere;
+	
+		private Rigidbody _rigidbody;
+		private Transform _playerCameraTransform;
+		private bool _isGrounded;
+	
+		private Vector3 _moveVector;
+	
+		private void Start() {
+			_rigidbody = gameObject.GetComponent<Rigidbody>();
+			_playerCameraTransform = playerCamera.transform;
+		}
+	
+		private void Update() {
+			ReadMovementInput();
+			CheckIfPlayerIsGrounded();
+			RealignPlayerToCameraForward();
+			MoveCameraWithPlayer();
+		}
+
+		private void ReadMovementInput() {
+			var userInput = new Vector2 {
+				x = Input.GetAxisRaw("Horizontal"),
+				y = Input.GetAxisRaw("Vertical")
+			};
+			//clamping so that diagonal move isn't faster than forward/sideways move by themselves
+			userInput = Vector2.ClampMagnitude(userInput, 1f);
+			userInput *= moveSpeed * Time.deltaTime;
+
+			var moveToApplyThisFrame = ConvertPlayerInputToWorldspace(userInput);
+			_moveVector += moveToApplyThisFrame;
+			//todo: method name obfuscates function. The method not only reads input, but parses to it to worldspace and add to input cache (in between fixedUpdate calls)
+		}
+
+		private Vector3 ConvertPlayerInputToWorldspace(Vector2 playerInput) {
+			var playerInputVector3 = new Vector3(playerInput.x, 0f, playerInput.y);
+			var worldspaceMove = transform.TransformDirection(playerInputVector3);
+			return worldspaceMove;
+		}
+
+		private void CheckIfPlayerIsGrounded() {
+			_isGrounded = Physics
+				.CheckSphere(groundChecker.position, groundCheckDistance, LayerMask.GetMask("Ground"));
+		}
+	
+		private void RealignPlayerToCameraForward() {
+			//todo change into generic usage Extension Method
+			//realignes player up to upwardsDirection (exactly) and player forward to camera forward (as close as possible)
+			var playerCameraForward = _playerCameraTransform.forward;
+			var newPlayerRot = Quaternion.LookRotation(Vector3.up, -playerCameraForward)
+			                   * Quaternion.AngleAxis(90f, Vector3.right);
+			transform.localRotation = newPlayerRot;
+		}
+	
+		private void MoveCameraWithPlayer() {
+			_playerCameraTransform.position = cameraAttachPoint.position;
+		}
+	
+		private void FixedUpdate() {
+			ApplyPlayerMovement();
+			if (Input.GetKey(KeyCode.Space) && _isGrounded) {
+				ApplyPlayerJump();
+			}
+		}
+
+		private void ApplyPlayerMovement() {
+			_rigidbody.MovePosition(_rigidbody.position + _moveVector);
+			_moveVector = Vector2.zero;
+		}
+
+		private void ApplyPlayerJump() {
+			_rigidbody.AddForce(transform.up * jumpForce, ForceMode.VelocityChange);
+		}
+	
+		private void OnDrawGizmos() {
+			Gizmos.color = Color.red;
+			if (drawGroundCheckSphere) {
+				Gizmos.DrawWireSphere(groundChecker.position,groundCheckDistance);
+			}
+		}
+	}
+}
